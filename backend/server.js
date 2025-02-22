446ae56adb5175e62a2ee638a7971a2bb9777dda
Shengli Shao <93632952+laodie666@users.noreply.github.com>
2025-02-22 14:13:19 -0500

    Update server.js
    Previous implementation was not so bright.

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,7 @@ import express from 'express'; // Importing express
 import { readFile } from './helper.js'; // Importing the readFile function from helper.js
 import cors from 'cors'; // Importing cors
 
-let bias_average = 0;
-let bias_variance = 0;
-let sites_visited = 0;
-let news_sites_visited = 0;
+let data = {};
 
 
 const app = express(); // Creating an express app
@@ -25,27 +22,22 @@ app.get('/', async (req, res) => {
 
 app.get('/data', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({"bias_average": bias_average, "bias_variance": bias_variance, "sites_visited": sites_visited, "news_sites_visited": news_sites_visited}));
+  res.end(JSON.stringify(data));
 });
-
 // req should be a json object matching the 4 constants above
 app.post('/update', (req, res) => {
   const request = req.body; // Correctly access the request body
   
  
   console.log(request);
-  if (request === undefined || request.bias_average === undefined || request.bias_variance === undefined || request.sites_visited === undefined || request.news_sites_visited === undefined) {
+  if (request === undefined) {
     res.status(400).send("Invalid request");
     return;
   }
-  bias_average = request.bias_average;
-  bias_variance = request.bias_variance;
-  sites_visited = request.sites_visited;
-  news_sites_visited = request.news_sites_visited;
+  data = request;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({bias_average: bias_average, bias_variance: bias_variance, sites_visited: sites_visited, news_sites_visited: news_sites_visited}));
+  res.end(JSON.stringify(data));
 });
-
 
 // Set up the server to listen on port 3000
 const port = 3000;
